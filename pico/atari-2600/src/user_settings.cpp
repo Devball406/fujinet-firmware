ccd05c5e0fdd77785437e5ea04919597d76acf0d
ARKBADUBUH406 <59310918+Devball406@users.noreply.github.com>
2025-04-04 02:22:15 -0500

    Delete pico directory

--- pico/atari-2600/src/user_settings.cpp
+++
@@ -1,3 +0,0 @@
-#include "user_settings.h"
-
-USER_SETTINGS user_settings = { TV_MODE_PAL, FONT_DEFAULT, SPACING_DEFAULT };
--- pico/coco/rom.h
+++
@@ -1,6 +0,0 @@
-#ifndef ROM_H
-#define ROM_H
-
-extern unsigned char rom[8192]; // Declare the array in the header file
-
-#endif
